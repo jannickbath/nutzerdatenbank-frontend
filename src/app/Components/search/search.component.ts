72882fb7acaf22c359cf3688aa9ed191c6f37082
Jannick Bath <jannick.bath@lupcom.de>
2024-08-15 12:46:11 +0200

    +add recommendations

--- src/app/Components/search/search.component.ts
+++ src/app/Components/search/search.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
-
+  public fields: Array<string> = ["E-Mail", "Nachname", "Vorname", "Straße"];
 }
