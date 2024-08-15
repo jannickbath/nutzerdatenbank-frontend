0b50adfc5e024453f8ac461f9f9b966dc1257dcf
Jannick Bath <jannick.bath@lupcom.de>
2024-08-15 12:28:18 +0200

    +search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchComponent } from './Components/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
