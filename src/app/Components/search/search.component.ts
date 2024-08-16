131da5a46633e2ffc28b433032452738e1df6e90
Jannick Bath <jannick.bath@lupcom.de>
2024-08-16 15:50:07 +0200

    +filter categories

--- src/app/Components/search/search.component.ts
+++ src/app/Components/search/search.component.ts
@@ -5,6 +5,7 @@ import { ApiUserResponse, User } from '../../Types';
 
 type Field = {
   name: string;
+  value: string;
   active: boolean;
 }
 
@@ -19,18 +20,22 @@ export class SearchComponent {
   public fields: Array<Field> = [
     {
       name: "E-Mail",
+      value: "email",
       active: false
     },
     {
       name: "Vorname",
+      value: "first_name",
       active: false
     },
     {
       name: "Nachname",
+      value: "last_name",
       active: false
     }
   ];
   public users: Array<User> = [];
+  private _search = "";
 
   constructor (public apiService: ApiService) {
     this.fetchUsers();
@@ -38,13 +43,8 @@ export class SearchComponent {
 
   public handleInput(event: Event) {
     const target = event.target as HTMLInputElement;
-    const value = target.value;
-
-    if (value) {
-      this.fetchUsers(target.value);
-    }else {
-      this.fetchUsers();
-    }
+    this._search = target.value;   
+    this.fetchUsers(); 
   }
 
   public toggleField(event: Event) {
@@ -54,11 +54,19 @@ export class SearchComponent {
     if (index) {
       this.fields[index].active = !this.fields[index].active;
     }
+
+    this.fetchUsers();
   }
 
-  private fetchUsers(search: string | null = null): void {
+  private fetchUsers(): void {
     const baseUrl = "http://nutzerdatenbank-backend.loc/users";
-    const url = search ? (baseUrl + "?search=" + search) : baseUrl;
+    let url = this._search ? (baseUrl + "?search=" + this._search) : baseUrl;
+    const activeFields = this.fields.filter(field => field.active);
+
+    if (this._search && activeFields.length > 0) {
+      url += "&filter=";
+      url += activeFields.map(field => field.value).join(",");
+    }
 
     fetch(url)
       .then(res => res.json())
