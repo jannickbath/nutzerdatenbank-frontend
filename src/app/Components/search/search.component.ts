484010aede53c2a6653cfbc6a8dab26ed6cf473a
Jannick Bath <jannick.bath@lupcom.de>
2024-08-16 15:22:19 +0200

    +search

--- src/app/Components/search/search.component.ts
+++ src/app/Components/search/search.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 import { ApiService } from '../../Services/api.service';
 import { ApiUserResponse, User } from '../../Types';
 
+type Field = {
+  name: string;
+  active: boolean;
+}
+
 @Component({
   selector: 'app-search',
   standalone: true,
@@ -11,7 +16,20 @@ import { ApiUserResponse, User } from '../../Types';
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
-  public fields: Array<string> = ["E-Mail", "Nachname", "Vorname", "Straße"];
+  public fields: Array<Field> = [
+    {
+      name: "E-Mail",
+      active: false
+    },
+    {
+      name: "Vorname",
+      active: false
+    },
+    {
+      name: "Nachname",
+      active: false
+    }
+  ];
   public users: Array<User> = [];
 
   constructor (public apiService: ApiService) {
@@ -26,6 +44,15 @@ export class SearchComponent {
       this.fetchUsers(target.value);
     }else {
       this.fetchUsers();
+    }
+  }
+
+  public toggleField(event: Event) {
+    const target = event.target as HTMLLIElement;
+    const index = target.dataset["index"] as number | undefined;
+
+    if (index) {
+      this.fields[index].active = !this.fields[index].active;
     }
   }
 
